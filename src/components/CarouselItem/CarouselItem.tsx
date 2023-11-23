0a823326f2c8c81938f29101f59e19096d4f8bb0
Adrian Karp <Adrian.Karp@ibm.com>
2023-11-23 15:09:38 +0200

    separated styled components from CarouselItem component

--- src/components/CarouselItem/CarouselItem.tsx
+++ src/components/CarouselItem/CarouselItem.tsx
@@ -1,24 +1,5 @@
 import React from 'react';
-import { Paper } from '@mui/material';
-import styled from '@emotion/styled';
-
-const ImageContainer = styled(Paper)`
-  width: 100%;
-  padding-top: 56.25%;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
-`;
-
-const Image = styled.img`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 100%;
-  height: auto;
-  transform: translate(-50%, -50%);
-  object-fit: cover;
-`;
+import { ImageContainer, Image } from './CarouselItem.styled';
 
 interface CarouselItemProps {
   image: string;
